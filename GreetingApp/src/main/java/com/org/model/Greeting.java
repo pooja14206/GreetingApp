cd0630a1d4bab93e3ad92c49709998226c37ad44
Pooja14206 <pooja.14206@gmail.com>
2021-08-10 21:31:11 +0530

    added a file for greeting app

---
+++ GreetingApp/src/main/java/com/org/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.org.model;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+    private long id;
+    private String message;
+
+    public Greeting(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+}
